28cd2b4b033e49f0a565382d7e41edc1bd29e64a
fsaiz97 <felixsaiz97@gmail.com>
2022-10-04 14:45:09 +0100

    Create dog-generator.js Add generateRandomDog function

---
+++ dog-generator.js
@@ -0,0 +1,49 @@
+// Write a function to generate dogs with random values for each property (Optional: Restrict property values based on each other)
+
+const {randomIndex, randomInteger, randomBoolean} = require("./random-functions")
+
+const traits = [
+    "Active",
+    "Intelligent",
+    "Playful",
+    "Curious",
+    "Loyal",
+    "Friendly",
+    "Sociable",
+    "Affectionate",
+    "Energetic",
+    "Lively"
+]
+const healthConditions = ["Healthy", "Diabetic", "Gingivitis"]
+
+function generateRandomDog() {
+    const breed = breedList(randomIndex(breedList));
+    const age = randomInteger(0, 20);
+    const good_with_cats = randomBoolean();
+    const good_with_dogs = randomBoolean();
+    const temperament = [];
+    // Generate 4 random traits and add to temperament
+    for (index of randomIndices(traits, 4)) {
+        temperament.push(traits[index]);
+    }
+    const good_with_kids = randomBoolean();
+    const good_with_elderly = randomBoolean();
+    const good_with_strangers = randomBoolean();
+    const independent = randomBoolean();
+    const hypoallergenic = randomBoolean();
+    const health_status = healthConditions[randomIndex(healthConditions)];
+
+    return {
+        breed,
+        age,
+        good_with_cats,
+        good_with_dogs,
+        temperament,
+        good_with_kids,
+        good_with_elderly,
+        good_with_strangers,
+        independent,
+        hypoallergenic,
+        health_status
+    }
+}
