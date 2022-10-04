c377ad00b59c59b5bdf0467aa2ec97b425b9a874
fsaiz97 <felixsaiz97@gmail.com>
2022-10-04 10:08:39 +0100

    Setup basic app to listen on port 3000

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 // Main code for the server app
 // Will use express.js
+const express = require("express");
+const cors = require("cors");
+
+function middleSetup(app) {
+    app.use(cors()); // Allow cross-origin requests
+    app.use(express.json()); // Allow app to read the body of POST requests
+}
+
+const app = express();
+
+
+middleSetup(app);
+
+module.exports = { app }
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // File to run the server on
+const { app } = require("./app")
+
+const port = 3000; // hardcoded port
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}...`)
+})
