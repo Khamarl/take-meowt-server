dd0de8be71ed283fe7d57fd1a76792012b564774
fsaiz97 <felixsaiz97@gmail.com>
2022-10-04 17:36:11 +0100

    Update matching-algorithm.js Add TODO comments detailing flow of the matching algorithm

---
+++ matching-algorithm.js
@@ -0,0 +1,8 @@
+// TODO For each form input, give each animal a match if its characteristics meets the criteria for that input
+
+// TODO Then sort the list of animals from the most number of matches to the least number of matches
+
+// TODO Then filter the sorted list by the user's pet preferences (breed, size, temperament, etc...)
+
+// TODO Return the filtered and sorted list
+
